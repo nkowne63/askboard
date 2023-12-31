791962741328a40e682881cf7fa13880e71c25cc
nkowne63rt <neutron63zf@gmail.com>
2024-01-01 00:21:12 +0900

    add: typescript

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,10 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-} from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
 
 export default function App() {
   return (
     <html>
       <head>
-        <link
-          rel="icon"
-          href="data:image/x-icon;base64,AA"
-        />
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
         <Meta />
         <Links />
       </head>
