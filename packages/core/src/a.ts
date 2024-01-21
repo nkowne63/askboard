a26895cb13307e8e0fc9769939fd98c187ff3d7c
nkowne63rt <neutron63zf@gmail.com>
2024-01-21 23:05:49 +0900

    add: basic types

--- packages/core/src/a.ts
+++ packages/core/src/a.ts
@@ -1,1 +1,23 @@
 export const add = (a: number, b: number) => a + b;
+import { OpenAI as Client } from "openai";
+
+const ApiKeySymbol = Symbol("ApiKey");
+type ApiKeySymbol = typeof ApiKeySymbol;
+
+type ApiKey = string & { [ApiKeySymbol]: never };
+
+export const parseApiKey = (apiKey: string): ApiKey => {
+  if (!apiKey) {
+    throw new Error("API key is required");
+  }
+  return apiKey as ApiKey;
+}
+
+type ThreadsRepository = {}
+type AssistantsRepository = {}
+
+export type ClientInfo = {
+    client: Client,
+    threads: ThreadsRepository
+    assistants: AssistantsRepository
+}
